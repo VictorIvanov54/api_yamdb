9a72aea26ee5d8c637d7cbb98b7d9a546a78e229
Gayratjon <gayratjon.kuziyev@mail.ru>
2025-04-21 01:25:04 +0300

    review/comments

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Review, Comment
 
-# Register your models here.
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('title', 'text', 'score', 'author', 'pub_date')
+    list_filter = ('title', 'author', 'score', 'pub_date')
+    search_fields = ('text', 'author__username', 'title__name')
+    date_hierarchy = 'pub_date'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('review', 'text', 'author', 'pub_date')
+    list_filter = ('review', 'author', 'pub_date')
+    search_fields = ('text', 'author__username', 'review__title__name')
+    date_hierarchy = 'pub_date'
--- api_yamdb/reviews/urls.py
+++ api_yamdb/reviews/urls.py
@@ -1,17 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ReviewViewSet, CommentViewSet # Импорт из reviews!
+from .views import ReviewViewSet, CommentViewSet
 
 router = DefaultRouter()
 
 router.register(r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet,
                 basename='review')
-
-comments_router = DefaultRouter()
-comments_router.register(r'reviews/(?P<review_id>[^/.]+)/comments',
-                         CommentViewSet, basename='review-comments')
+router.register(r'reviews/(?P<review_id>[^/.]+)/comments',
+                CommentViewSet, basename='comment')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('', include(comments_router.urls)),
 ]
