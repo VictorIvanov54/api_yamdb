a82253add47a9651a83e4b213c5971caa0c96923
Maria Kolesnikova <maria.wheels28@gmail.com>
2025-04-25 09:41:07 -0700

    Add mixin to serializer

--- api_yamdb/api/mixins.py
+++ api_yamdb/api/mixins.py
@@ -22,20 +22,6 @@ class ListCreateDeleteViewSet(
 class UserValidationMixin:
     """Миксин с валидацией имени пользователя и email."""
 
-    # def validate_username(self, value):
-    #     if value.lower() == 'me':
-    #         raise serializers.ValidationError('Имя "me" запрещено.')
-    #     if User.objects.filter(username=value).exists():
-    #         raise serializers.ValidationError(
-    #             'Это имя пользователя уже используется.'
-    #         )
-    #     return value
-
-    # def validate_email(self, value):
-    #     if User.objects.filter(email=value).exists():
-    #         raise serializers.ValidationError('Этот email уже используется.')
-    #     return value
-
     def validate_username(self, value):
         if value.lower() == 'me':
             raise serializers.ValidationError('Имя "me" запрещено.')
